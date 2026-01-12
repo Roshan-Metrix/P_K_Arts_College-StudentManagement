bd3d9f4b71ae8657b2049d2d7fcb47f44170520b
Roshan Patel <roshanpatel12309@gmail.com>
2026-01-12 19:26:39 +0530

    Added route for college

--- backend/routes/collegeRoutes.js
+++ backend/routes/collegeRoutes.js
@@ -1,18 +1,46 @@
-import express from 'express'
-import userAuth from '../middleware/userAuth.js';
-import { AddCollegeEvents, AddCollegeEventsGallery, getCollegeEvents, getCollegeEventsGallery } from '../controllers/collegeController.js';
+import express from "express";
+import userAuth from "../middleware/userAuth.js";
+import {
+  AddCollegeEvents,
+  AddCollegeEventsGallery,
+  AddCoursesFiles,
+  getCollegeEvents,
+  getCollegeEventsGallery,
+  getCoursesFiles,
+} from "../controllers/collegeController.js";
+import uploadEventImages from "../middleware/uploadEventImage.js";
+import uploadCoursePdf from "../middleware/uploadCoursePdf.js";
 
 const collegeRouter = express.Router();
 
-collegeRouter.get('/',(req,res) => {
-    res.send("College Route Working . . .")
+collegeRouter.get("/", (req, res) => {
+  res.send("College Route Working . . .");
 });
 
-collegeRouter.post('/add-events', userAuth, AddCollegeEvents)
-collegeRouter.get('/get-events', getCollegeEvents)
-collegeRouter.post('/add-event-images', userAuth, AddCollegeEventsGallery)
-collegeRouter.get('/get-event-images', getCollegeEventsGallery)
+// Adding & Fetching events for the calendar
+collegeRouter.post("/add-events", userAuth, AddCollegeEvents);
+collegeRouter.get("/get-events", getCollegeEvents);
 
+// Adding & Fetching Events images, name , desc
+collegeRouter.post(
+  "/add-event-images",
+  userAuth,
+  uploadEventImages.fields([
+    { name: "photo1", maxCount: 1 },
+    { name: "photo2", maxCount: 1 },
+    { name: "photo3", maxCount: 1 },
+  ]),
+  AddCollegeEventsGallery
+);
+collegeRouter.get("/get-event-images", getCollegeEventsGallery);
 
+// Adding & Fetching Events courses
+collegeRouter.post(
+  "/add-course-files",
+  userAuth,
+  uploadCoursePdf.single("file"),
+  AddCoursesFiles
+);
+collegeRouter.get("/get-course-files/:sub_name",getCoursesFiles);
 
 export default collegeRouter;
